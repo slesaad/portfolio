ac74dbd90991c6c9ae3cf7f63db0dde580f8e2f8
Slesa Adhikari <sa0112@uah.edu>
2019-09-10 23:46:18 -0500

    Implement static portfolio webpage

--- resume/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Education, Experience, Skill, Interest
-
-
-admin.site.register(Education)
-admin.site.register(Experience)
-admin.site.register(Skill)
-admin.site.register(Interest)
--- run_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sass --watch stylesheets:static/css &
-python manage.py runserver 0.0.0.0:8000
