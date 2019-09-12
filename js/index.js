3a8343b0d10e55847d52db14d9e2d0ced035134c
Slesa Adhikari <slesaad@gmail.com>
2019-09-12 17:38:55 -0500

    Add timeline and redo projects page

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 $(document).ready(() => {
     $('.project-item').on('click', (e) => {
-        const item = $(e.target);
+        const item = $(e.currentTarget);
         if (item.hasClass('details')) {
             item.removeClass('details');
         } else {
+            $('.project-item').removeClass('details');
             item.addClass('details');
         }
     });
