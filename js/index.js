e8e798b820e3ce5dc764bd428e4f576179094c8f
Slesa Adhikari <sa0112@uah.edu>
2019-09-12 01:22:11 -0500

    Add experiences and contact pages

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 $(document).ready(() => {
+    $('.project-item').on('click', (e) => {
+        const item = $(e.target);
+        if (item.hasClass('details')) {
+            item.removeClass('details');
+        } else {
+            item.addClass('details');
+        }
+    });
 });
